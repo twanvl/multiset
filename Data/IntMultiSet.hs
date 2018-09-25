f3cc4a353d87bc22bc68502d9e382408ea7d90f1
Twan van Laarhoven <twanvl@gmail.com>
2018-09-25 23:16:43 +0200

    Use MIN_VERSION_containers

--- Data/IntMultiSet.hs
+++ Data/IntMultiSet.hs
@@ -145,7 +145,7 @@ import Data.Typeable ()
 import Data.IntMap.Strict (IntMap)
 import Data.IntSet (IntSet)
 import Data.MultiSet (MultiSet)
-#if __GLASGOW_HASKELL__ >= 806
+#if MIN_VERSION_containers(0,5,11)
 import qualified Data.IntMap.Strict as Map hiding (showTreeWith)
 import qualified Data.IntMap.Internal.Debug as Map (showTreeWith)
 #else
